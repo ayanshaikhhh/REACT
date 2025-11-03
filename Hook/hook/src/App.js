33b8b71d892af1dd935f2063fc3e2d09774e1528
mohammed ayan <mohammedayan71636@gmail.com>
2025-11-03 23:51:22 +0530

    update code

---
+++ Hook/hook/src/App.js
@@ -0,0 +1,32 @@
+// import logo from './logo.svg';
+import './App.css';
+import ClassCounter from './components/ClassCounter';
+import HookCounter from './components/HookCounter';
+
+
+
+
+function App() {
+  return (
+    <div className="App">
+      return (
+        <div className='App'>
+          {/* <ClassCounter/> */}
+          <HookCounter/>
+
+        </div>
+      )
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
