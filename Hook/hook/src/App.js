495d4f8bfff2e349c6c46b6c0070bb2e070b2e43
mohammed ayan <mohammedayan71636@gmail.com>
2025-11-04 11:25:02 +0530

    update code

--- Hook/hook/src/App.js
+++ Hook/hook/src/App.js
@@ -2,20 +2,37 @@
 import './App.css';
 import ClassCounter from './components/ClassCounter';
 import HookCounter from './components/HookCounter';
-
+import HookCounterTwo from './components/HookCounterTwo';
+import HookCoounterThree from './components/HookCoounterThree';
+import HookCounterFour from './components/HookCounterFour';
 
 
 
 function App() {
   return (
     <div className="App">
-      return (
         <div className='App'>
-          {/* <ClassCounter/> */}
-          <HookCounter/>
 
+
+
+
+
+
+
+          {/* <ClassCounter/> */}
+          {/* <HookCounter/> */}
+          {/* <HookCounterTwo/> */}
+          {/* <HookCoounterThree/> */}
+          <HookCounterFour/>
+       
+       
+
+
+       
+       
+       
+       
         </div>
-      )
     </div>
   );
 }
