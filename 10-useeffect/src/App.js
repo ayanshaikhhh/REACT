c6d6120d027a7adb8a0626104b01f14d29a80a9f
mohammed ayan <mohammedayan71636@gmail.com>
2025-11-17 09:11:55 +0530

    update

---
+++ 10-useeffect/src/App.js
@@ -0,0 +1,18 @@
+// import logo from './logo.svg';
+import './App.css';
+import { useEffect } from 'react';
+
+function App() {
+  
+
+  
+
+  useEffect(() => {
+    alert('Hello, World!');
+    
+  }, [])
+  
+}
+
+export default App;
+
